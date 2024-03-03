d89aacfe144716922dee4c719a8f8b1da3be8e89
FredNekrasov <frednekrasov49@gmail.com>
2024-03-03 18:41:46 +0300

    creating the app class

---
+++ app/src/main/java/com/mytests/App.kt
@@ -0,0 +1,18 @@
+package com.mytests
+
+import android.app.Application
+import com.mytests.test_exam.di.*
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            androidLogger()
+            modules(dataExamModule,domainExamModule,presentationExamModule)
+        }
+    }
+}
