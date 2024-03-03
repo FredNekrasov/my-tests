5e6c44cbe17668c4ffad5e2297c652b2ec4050b1
FredNekrasov <frednekrasov49@gmail.com>
2024-03-03 17:57:59 +0300

    creating mappers to map an entity to model

---
+++ app/src/main/java/com/mytests/test_exam/data/local/entities/AnimalFactsEntity.kt
@@ -0,0 +1,16 @@
+package com.mytests.test_exam.data.local.entities
+
+import androidx.room.*
+import com.mytests.test_exam.domain.model.AnimalFacts
+
+@Entity
+data class AnimalFactsEntity(
+    val text: String,
+    val animalType: String,
+    val amount: Int,
+    val isFavorite: Boolean,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+) {
+    fun toDomain() = AnimalFacts(text, animalType, isFavorite)
+}
