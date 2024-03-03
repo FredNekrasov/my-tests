14fb418092b06b593476443f060abfdfc25a0267
FredNekrasov <frednekrasov49@gmail.com>
2024-03-03 18:25:55 +0300

    creating a database

---
+++ app/src/main/java/com/mytests/test_exam/data/local/MainDB.kt
@@ -0,0 +1,21 @@
+package com.mytests.test_exam.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.mytests.test_exam.data.local.dao.IAnimalFactsDao
+import com.mytests.test_exam.data.local.dao.IUserDao
+import com.mytests.test_exam.data.local.entities.AnimalFactsEntity
+import com.mytests.test_exam.data.local.entities.UserEntity
+
+@Database(
+    entities = [UserEntity::class,AnimalFactsEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MainDB : RoomDatabase() {
+    abstract val userDao: IUserDao
+    abstract val animalFactsDao: IAnimalFactsDao
+    companion object {
+        const val DATABASE_NAME = "exam_db"
+    }
+}
