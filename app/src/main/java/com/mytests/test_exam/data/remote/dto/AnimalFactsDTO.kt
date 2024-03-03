85b722aee94a0671f34195add377991d269dde12
FredNekrasov <frednekrasov49@gmail.com>
2024-03-03 17:49:54 +0300

    creating a dto class

---
+++ app/src/main/java/com/mytests/test_exam/data/remote/dto/AnimalFactsDTO.kt
@@ -0,0 +1,10 @@
+package com.mytests.test_exam.data.remote.dto
+
+
+data class AnimalFactsDTO(
+    val text: String,
+    val type: String,
+    val deleted: Boolean,
+    val createdAt: String,
+    val updatedAt: String
+)
