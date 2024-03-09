92f00b74d8966070fa2e050b8284bbf977f28f76
FredNekrasov <frednekrasov49@gmail.com>
2024-03-09 23:10:28 +0300

    removing a Column from the ExamScaffold

--- app/src/main/java/com/mytests/testExam/presentation/ExamScaffold.kt
+++ app/src/main/java/com/mytests/testExam/presentation/ExamScaffold.kt
@@ -1,9 +1,9 @@
 package com.mytests.testExam.presentation
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.navigation.compose.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mytests.testExam.presentation.list.DataList
 import com.mytests.testExam.presentation.list.FavouriteList
 import com.mytests.testExam.presentation.list.view_model.AnimalFactsVM
@@ -17,13 +17,11 @@ fun ExamScaffold(displayMessage: (String) -> Unit) {
     val controller = rememberNavController()
     val viewModel: AnimalFactsVM = koinViewModel()
     val userVM: UserVM = koinViewModel()
-    Column(Modifier.fillMaxSize(),Arrangement.Center,Alignment.CenterHorizontally) {
-        NavHost(controller, startDestination = ExamScreenRoutes.Authorization.route) {
-            composable(ExamScreenRoutes.Authorization.route) { Authorization(controller,userVM) }
-            composable(ExamScreenRoutes.Registration.route) { Registration(controller,userVM) }
-            composable(ExamScreenRoutes.Profile.route) { Profile(controller,userVM,displayMessage) }
-            composable(ExamScreenRoutes.DataList.route) { DataList(controller,viewModel) }
-            composable(ExamScreenRoutes.Favorites.route) { FavouriteList(controller,viewModel) }
-        }
+    NavHost(controller, ExamScreenRoutes.Authorization.route) {
+        composable(ExamScreenRoutes.Authorization.route) { Authorization(controller,userVM) }
+        composable(ExamScreenRoutes.Registration.route) { Registration(controller,userVM) }
+        composable(ExamScreenRoutes.Profile.route) { Profile(controller,userVM,displayMessage) }
+        composable(ExamScreenRoutes.DataList.route) { DataList(controller,viewModel) }
+        composable(ExamScreenRoutes.Favorites.route) { FavouriteList(controller,viewModel) }
     }
 }
