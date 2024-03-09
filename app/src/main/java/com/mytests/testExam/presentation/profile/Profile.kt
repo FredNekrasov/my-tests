2b1129429abfb21aa7d49aa9293798fd6e27d90f
FredNekrasov <frednekrasov49@gmail.com>
2024-03-09 23:18:57 +0300

    refactoring the Profile screen

--- app/src/main/java/com/mytests/testExam/presentation/profile/Profile.kt
+++ app/src/main/java/com/mytests/testExam/presentation/profile/Profile.kt
@@ -10,26 +10,27 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.mytests.R.string
+import com.mytests.testExam.domain.util.AuthStatus.DELETED
 import com.mytests.testExam.presentation.profile.view_model.UserVM
 import com.mytests.ui.customItems.*
 import com.mytests.ui.screenRoutes.ExamScreenRoutes
 
 @Composable
 fun Profile(controller: NavHostController,userVM: UserVM,displayMessage: (String) -> Unit) {
-    val user = userVM.resultSF.collectAsState().value.second
+    val state = userVM.resultSF.collectAsState().value
     val deleteMessage = stringResource(string.delete)
     Column(Modifier.fillMaxSize(),Arrangement.Top,Alignment.CenterHorizontally) {
         FredTextHeader(stringResource(string.profile))
         Spacer(Modifier.height(16.dp))
-        UserInfo(user, {
-            userVM.delete()
-            controller.popBackStack(ExamScreenRoutes.Authorization.route, false)
-            displayMessage(deleteMessage)
-        }, Modifier.fillMaxWidth())
+        UserInfo(state.second,{ userVM.delete() },Modifier.fillMaxWidth())
         Spacer(Modifier.height(16.dp))
         Row(Modifier.fillMaxWidth(), Arrangement.SpaceAround, Alignment.CenterVertically) {
             FredIconButton({ controller.navigate(ExamScreenRoutes.DataList.route) },Outlined.Dataset, stringResource(string.facts))
             FredIconButton({ controller.navigate(ExamScreenRoutes.Favorites.route) }, Outlined.Favorite, stringResource(string.favourites))
+        }
+        if(state.first == DELETED) {
+            controller.popBackStack(ExamScreenRoutes.Authorization.route, false)
+            displayMessage(deleteMessage)
         }
     }
 }
