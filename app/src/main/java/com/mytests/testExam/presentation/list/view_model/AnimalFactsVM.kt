cd67135dc956c20d4c6071819a35e1fa871ef14a
FredNekrasov <frednekrasov49@gmail.com>
2024-03-09 23:08:53 +0300

    refactoring the AnimalFactsVM.kt

--- app/src/main/java/com/mytests/testExam/presentation/list/view_model/AnimalFactsVM.kt
+++ app/src/main/java/com/mytests/testExam/presentation/list/view_model/AnimalFactsVM.kt
@@ -2,12 +2,12 @@ package com.mytests.testExam.presentation.list.view_model
 
 import androidx.lifecycle.*
 import com.mytests.testExam.domain.model.AnimalFacts
-import com.mytests.testExam.domain.useCases.IAnimalFactsUseCase
+import com.mytests.testExam.domain.useCases.animalFacts.AnimalFactsUseCases
 import com.mytests.testExam.domain.util.Status
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.*
 
-class AnimalFactsVM(private val animalFactsUseCase: IAnimalFactsUseCase) : ViewModel() {
+class AnimalFactsVM(private val useCases: AnimalFactsUseCases) : ViewModel() {
     private val resultMSF = MutableStateFlow(Status.NONE to emptyList<AnimalFacts>())
     val resultSF = resultMSF.asStateFlow()
     init {
@@ -16,14 +16,14 @@ class AnimalFactsVM(private val animalFactsUseCase: IAnimalFactsUseCase) : ViewM
     fun getData(animalType: String = "cat",amount: Int = 2) {
         viewModelScope.launch {
             resultMSF.emit(Status.LOADING to emptyList())
-            animalFactsUseCase.getAnimalFacts(animalType,amount).collectLatest {
+            useCases.getFacts.getAnimalFacts(animalType,amount).collectLatest {
                 resultMSF.emit(it)
             }
         }
     }
     fun updateData(animalFacts: AnimalFacts) {
         viewModelScope.launch {
-            animalFactsUseCase.updateFact(animalFacts)
+            useCases.updateFact.updateFact(animalFacts)
         }
     }
 }
