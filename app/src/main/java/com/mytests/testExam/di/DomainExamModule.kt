77cda3e02adc858643948e26f9e9323a44623f80
FredNekrasov <frednekrasov49@gmail.com>
2024-03-09 23:09:45 +0300

    creating a di for use cases

--- app/src/main/java/com/mytests/testExam/di/DomainExamModule.kt
+++ app/src/main/java/com/mytests/testExam/di/DomainExamModule.kt
@@ -1,12 +1,27 @@
 package com.mytests.testExam.di
 
-import com.mytests.testExam.domain.useCases.IAnimalFactsUseCase
-import com.mytests.testExam.domain.useCases.IUserUseCase
-import com.mytests.testExam.domain.useCases.implementation.AnimalFactsUseCase
-import com.mytests.testExam.domain.useCases.implementation.UserUseCase
+
+import com.mytests.testExam.domain.useCases.animalFacts.AnimalFactsUseCases
+import com.mytests.testExam.domain.useCases.animalFacts.get.IGetAnimalFactsUseCase
+import com.mytests.testExam.domain.useCases.animalFacts.get.implementation.GetAnimalFactsUseCase
+import com.mytests.testExam.domain.useCases.animalFacts.update.IUpdateFactUseCase
+import com.mytests.testExam.domain.useCases.animalFacts.update.implementation.UpdateFactUseCase
+import com.mytests.testExam.domain.useCases.user.UserUseCases
+import com.mytests.testExam.domain.useCases.user.authorization.IAuthorizationUseCase
+import com.mytests.testExam.domain.useCases.user.authorization.implementation.AuthorizationUseCase
+import com.mytests.testExam.domain.useCases.user.delete.IDeleteUserUseCase
+import com.mytests.testExam.domain.useCases.user.delete.implementation.DeleteUserUseCase
+import com.mytests.testExam.domain.useCases.user.registration.IRegistrationUseCase
+import com.mytests.testExam.domain.useCases.user.registration.implementation.RegistrationUseCase
 import org.koin.dsl.module
 
 val domainExamModule = module {
-    factory<IUserUseCase> { UserUseCase(get()) }
-    factory<IAnimalFactsUseCase> { AnimalFactsUseCase(get()) }
+    factory<IAuthorizationUseCase> { AuthorizationUseCase(get()) }
+    factory<IRegistrationUseCase> { RegistrationUseCase(get()) }
+    factory<IDeleteUserUseCase> { DeleteUserUseCase(get()) }
+    factory { UserUseCases(get(), get(), get()) }
+
+    factory<IGetAnimalFactsUseCase> { GetAnimalFactsUseCase(get()) }
+    factory<IUpdateFactUseCase> { UpdateFactUseCase(get()) }
+    factory { AnimalFactsUseCases(get(), get()) }
 }
