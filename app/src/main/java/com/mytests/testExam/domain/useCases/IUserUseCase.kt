555ccceea971c8ab84a31ad72fbc0f6eb32ba5c3
FredNekrasov <frednekrasov49@gmail.com>
2024-03-09 22:57:52 +0300

    deleting files UserUseCase.kt and IUserUseCase.kt

--- app/src/main/java/com/mytests/testExam/domain/useCases/IUserUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.mytests.testExam.domain.useCases
-
-import com.mytests.testExam.domain.model.User
-
-interface IUserUseCase {
-    suspend fun authorization(userName: String, password: String): User?
-    suspend fun registration(user: User): User?
-    suspend fun deleteUser(user: User)
-}
