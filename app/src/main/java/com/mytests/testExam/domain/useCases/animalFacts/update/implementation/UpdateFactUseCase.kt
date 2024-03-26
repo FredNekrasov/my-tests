327a2008527a1912dc6cd78f7abc025fe555c825
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:33:44 +0300

    refactoring the UpdateFactUseCase.kt

--- app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/update/implementation/UpdateFactUseCase.kt
+++ app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/update/implementation/UpdateFactUseCase.kt
@@ -4,6 +4,8 @@ import com.mytests.testExam.domain.model.AnimalFacts
 import com.mytests.testExam.domain.repository.IAnimalFactsRepository
 import com.mytests.testExam.domain.useCases.animalFacts.update.IUpdateFactUseCase
 
-class UpdateFactUseCase(private val repository: IAnimalFactsRepository) : IUpdateFactUseCase {
-    override suspend fun updateFact(animalFacts: AnimalFacts) = repository.updateEntity(animalFacts.toEntity())
+class UpdateFactUseCase(
+    private val repository: IAnimalFactsRepository
+) : IUpdateFactUseCase {
+    override suspend fun updateFact(animalFacts: AnimalFacts) = repository.updateEntity(animalFacts)
 }
