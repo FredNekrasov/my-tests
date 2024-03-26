df07c9c3841283daa9be52202d4a87918f8d7167
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:34:15 +0300

    refactoring the GetAnimalFactsUseCase

--- app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/get/IGetAnimalFactsUseCase.kt
+++ app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/get/IGetAnimalFactsUseCase.kt
@@ -1,9 +1,9 @@
 package com.mytests.testExam.domain.useCases.animalFacts.get
 
 import com.mytests.testExam.domain.model.AnimalFacts
-import com.mytests.testExam.domain.util.Status
+import com.mytests.testExam.domain.util.ConnectionStatus
 import kotlinx.coroutines.flow.StateFlow
 
 interface IGetAnimalFactsUseCase {
-    suspend fun getAnimalFacts(animalType: String, amount: Int): StateFlow<Pair<Status,List<AnimalFacts>>>
+    suspend fun getAnimalFacts(animalType : String, amount : Int): StateFlow<Pair<ConnectionStatus,List<AnimalFacts>>>
 }
--- app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/get/implementation/GetAnimalFactsUseCase.kt
+++ app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/get/implementation/GetAnimalFactsUseCase.kt
@@ -3,16 +3,13 @@ package com.mytests.testExam.domain.useCases.animalFacts.get.implementation
 import com.mytests.testExam.domain.model.AnimalFacts
 import com.mytests.testExam.domain.repository.IAnimalFactsRepository
 import com.mytests.testExam.domain.useCases.animalFacts.get.IGetAnimalFactsUseCase
-import com.mytests.testExam.domain.util.Status
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.mytests.testExam.domain.util.ConnectionStatus
 import kotlinx.coroutines.flow.StateFlow
 
-class GetAnimalFactsUseCase(private val repository: IAnimalFactsRepository) : IGetAnimalFactsUseCase {
+class GetAnimalFactsUseCase(
+    private val repository: IAnimalFactsRepository
+) : IGetAnimalFactsUseCase {
     override suspend fun getAnimalFacts(
-        animalType: String,amount: Int
-    ): StateFlow<Pair<Status,List<AnimalFacts>>> {
-        val (status, factsEntity) = repository.getList(animalType,amount).value
-        val facts = factsEntity.map { it.toDomain() }
-        return MutableStateFlow(status to facts)
-    }
+        animalType : String, amount : Int
+    ): StateFlow<Pair<ConnectionStatus,List<AnimalFacts>>> = repository.getList(animalType, amount)
 }
