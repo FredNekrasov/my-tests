4fb51b221dbf604458950507d040d98e6fcdbdd9
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:39:56 +0300

    refactoring Profile screen

--- app/src/main/java/com/mytests/testExam/presentation/profile/Profile.kt
+++ app/src/main/java/com/mytests/testExam/presentation/profile/Profile.kt
@@ -11,24 +11,25 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.mytests.R.string
 import com.mytests.testExam.domain.util.AuthStatus.DELETED
+import com.mytests.testExam.presentation.profile.view_model.UserEvent
 import com.mytests.testExam.presentation.profile.view_model.UserVM
 import com.mytests.ui.customItems.*
 import com.mytests.ui.screenRoutes.ExamScreenRoutes
 
 @Composable
-fun Profile(controller: NavHostController,userVM: UserVM,displayMessage: (String) -> Unit) {
+fun Profile(controller : NavHostController, userVM : UserVM, displayMessage : (String) -> Unit) {
     val state = userVM.resultSF.collectAsState().value
     val deleteMessage = stringResource(string.delete)
-    Column(Modifier.fillMaxSize(),Arrangement.Top,Alignment.CenterHorizontally) {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         FredTextHeader(stringResource(string.profile))
         Spacer(Modifier.height(16.dp))
-        UserInfo(state.second,{ userVM.delete() },Modifier.fillMaxWidth())
+        UserInfo(state.user, { userVM.onUserEvent(UserEvent.DeleteUser) }, Modifier.fillMaxWidth())
         Spacer(Modifier.height(16.dp))
         Row(Modifier.fillMaxWidth(), Arrangement.SpaceAround, Alignment.CenterVertically) {
-            FredIconButton({ controller.navigate(ExamScreenRoutes.DataList.route) },Outlined.Dataset, stringResource(string.facts))
+            FredIconButton({ controller.navigate(ExamScreenRoutes.DataList.route) }, Outlined.Dataset, stringResource(string.facts))
             FredIconButton({ controller.navigate(ExamScreenRoutes.Favorites.route) }, Outlined.Favorite, stringResource(string.favourites))
         }
-        if(state.first == DELETED) {
+        if(state.status == DELETED) {
             controller.popBackStack(ExamScreenRoutes.Authorization.route, false)
             displayMessage(deleteMessage)
         }
