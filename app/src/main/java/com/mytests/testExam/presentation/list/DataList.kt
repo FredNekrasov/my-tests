c996a2ea8a61a06064c3e40224f9466aa4b6254a
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:40:33 +0300

    refactoring the DataList screen

--- app/src/main/java/com/mytests/testExam/presentation/list/DataList.kt
+++ app/src/main/java/com/mytests/testExam/presentation/list/DataList.kt
@@ -1,51 +1,49 @@
 package com.mytests.testExam.presentation.list
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons.Outlined
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.mytests.R.string
 import com.mytests.testExam.domain.model.AnimalFacts
-import com.mytests.testExam.domain.util.Status
+import com.mytests.testExam.domain.util.ConnectionStatus
+import com.mytests.testExam.presentation.list.view_model.AnimalFactEvent
 import com.mytests.testExam.presentation.list.view_model.AnimalFactsVM
 import com.mytests.ui.customItems.*
 import com.mytests.ui.screenRoutes.ExamScreenRoutes
 
 @Composable
-fun DataList(controller: NavHostController,viewModel: AnimalFactsVM) {
-    val state = viewModel.resultSF.collectAsState().value
+fun DataList(controller : NavHostController, animalFactsVM : AnimalFactsVM) {
+    val state = animalFactsVM.resultSF.collectAsState().value
     var animalType by rememberSaveable { mutableStateOf("") }
     var amount by rememberSaveable { mutableStateOf("") }
     Box(Modifier.fillMaxSize()) {
-        if (state.first == Status.LOADING) CircularProgressIndicator(Modifier.align(Alignment.Center))
+        if (state.first == ConnectionStatus.LOADING) CircularProgressIndicator(Modifier.align(Alignment.Center))
         Column(Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally) {
             FredTextHeader(stringResource(string.facts))
             FredTextField(animalType,{ animalType = it }, stringResource(string.enterAnimalType))
             Spacer(Modifier.height(4.dp))
-            TextField(
+            FredTextField(
                 amount,
-                { amount = it },
-                placeholder = { Text(stringResource(string.enterAmount),fontFamily = FontFamily.Serif,color = MaterialTheme.colorScheme.onTertiaryContainer) },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                colors = OutlinedTextFieldDefaults.colors()
+                { if(amount.toIntOrNull() != null && amount.toInt() > 0) amount = it },
+                stringResource(string.enterAmount)
             )
             Spacer(Modifier.height(16.dp))
             Row(Modifier.fillMaxWidth(),Arrangement.SpaceAround,Alignment.CenterVertically) {
                 FredIconButton({ controller.navigateUp() },Outlined.ArrowBackIosNew, stringResource(string.goBack))
                 this@Box.ShowInternetInfo ({
-                    if(animalType.isNotEmpty() && amount.isNotEmpty()) viewModel.getData(animalType,amount.toInt()) else viewModel.getData()
+                    animalFactsVM.onEvent(AnimalFactEvent.GetAnimalFacts(animalType,amount.toInt()))
                 }, state.first)
                 FredIconButton({ controller.navigate(ExamScreenRoutes.Favorites.route) }, Outlined.FavoriteBorder,stringResource(string.favourites))
             }
@@ -54,9 +52,9 @@ fun DataList(controller: NavHostController,viewModel: AnimalFactsVM) {
                 items(state.second) { fact ->
                     var isFavorite by remember { mutableStateOf(fact.isFavorite) }
                     ListItem(fact, isFavorite, { it: AnimalFacts, isFav: Boolean ->
-                        viewModel.updateData(it)
                         isFavorite = isFav
                         fact.isFavorite = isFav
+                        animalFactsVM.onEvent(AnimalFactEvent.UpdateAnimalFacts(it))
                     }, Modifier.fillMaxWidth())
                     Spacer(Modifier.height(16.dp))
                 }
@@ -65,23 +63,23 @@ fun DataList(controller: NavHostController,viewModel: AnimalFactsVM) {
     }
 }
 @Composable
-fun BoxScope.ShowInternetInfo(action: () -> Unit,state: Status) {
+fun BoxScope.ShowInternetInfo(action : () -> Unit, state : ConnectionStatus) {
     val tryAgain = stringResource(string.tryAgain)
     TextButton(
         action,
         Modifier.align(Alignment.BottomCenter),
         enabled = when(state) {
-            Status.LOADING -> false
+            ConnectionStatus.LOADING -> false
             else -> true
         }
     ) {
         Text(
             when(state) {
-                Status.LOADING -> stringResource(string.LOADING)
-                Status.SUCCESS -> stringResource(string.SUCCESS)
-                Status.NO_DATA -> "${stringResource(string.NO_DATA)}\n${tryAgain}"
-                Status.ERROR -> "${stringResource(string.CONNECTION_ERROR)} or ${stringResource(string.NO_INTERNET)}\n${tryAgain}"
-                Status.NONE -> "get data"
+                ConnectionStatus.LOADING -> stringResource(string.LOADING)
+                ConnectionStatus.SUCCESS -> stringResource(string.SUCCESS)
+                ConnectionStatus.NO_DATA -> "${stringResource(string.NO_DATA)}\n${tryAgain}"
+                ConnectionStatus.ERROR -> "${stringResource(string.CONNECTION_ERROR)} or ${stringResource(string.NO_INTERNET)}\n${tryAgain}"
+                ConnectionStatus.NONE -> "get data"
             },textAlign = TextAlign.Center
         )
     }
