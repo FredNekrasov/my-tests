c6ab148e929c047f5c22b399c748071319bde64f
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:36:22 +0300

    refactoring the AnimalFactsVM

--- app/src/main/java/com/mytests/testExam/presentation/list/view_model/AnimalFactsVM.kt
+++ app/src/main/java/com/mytests/testExam/presentation/list/view_model/AnimalFactsVM.kt
@@ -3,27 +3,35 @@ package com.mytests.testExam.presentation.list.view_model
 import androidx.lifecycle.*
 import com.mytests.testExam.domain.model.AnimalFacts
 import com.mytests.testExam.domain.useCases.animalFacts.AnimalFactsUseCases
-import com.mytests.testExam.domain.util.Status
+import com.mytests.testExam.domain.util.ConnectionStatus
+import com.mytests.testExam.presentation.list.view_model.AnimalFactEvent.GetAnimalFacts
+import com.mytests.testExam.presentation.list.view_model.AnimalFactEvent.UpdateAnimalFacts
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.*
 
 class AnimalFactsVM(private val useCases: AnimalFactsUseCases) : ViewModel() {
-    private val resultMSF = MutableStateFlow(Status.NONE to emptyList<AnimalFacts>())
+    private val resultMSF = MutableStateFlow(ConnectionStatus.NONE to emptyList<AnimalFacts>())
     val resultSF = resultMSF.asStateFlow()
     init {
         getData()
     }
-    fun getData(animalType: String = "cat",amount: Int = 2) {
+    fun onEvent(event : AnimalFactEvent) {
+        when(event) {
+            is GetAnimalFacts -> getData(event.animalType, event.amount)
+            is UpdateAnimalFacts -> updateData(event.animalFacts)
+        }
+    }
+    private fun getData(animalType: String = "cat",amount: Int = 2) {
         viewModelScope.launch {
-            resultMSF.emit(Status.LOADING to emptyList())
-            useCases.getFacts.getAnimalFacts(animalType,amount).collectLatest {
+            resultMSF.emit(ConnectionStatus.LOADING to emptyList())
+            useCases.getAnimalFacts.getAnimalFacts(animalType,amount).collectLatest {
                 resultMSF.emit(it)
             }
         }
     }
-    fun updateData(animalFacts: AnimalFacts) {
+    private fun updateData(animalFacts: AnimalFacts) {
         viewModelScope.launch {
-            useCases.updateFact.updateFact(animalFacts)
+            useCases.updateAnimalFact.updateFact(animalFacts)
         }
     }
 }
