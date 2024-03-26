676931fa6c6af5b2903395038035c73f460d4758
FredNekrasov <frednekrasov49@gmail.com>
2024-03-26 22:39:18 +0300

    refactoring Registration screen

--- app/src/main/java/com/mytests/testExam/presentation/Registration.kt
+++ app/src/main/java/com/mytests/testExam/presentation/Registration.kt
@@ -3,7 +3,8 @@ package com.mytests.testExam.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -12,13 +13,14 @@ import com.mytests.R.string
 import com.mytests.testExam.domain.model.User
 import com.mytests.testExam.domain.util.AuthStatus.EXISTING_USER
 import com.mytests.testExam.domain.util.AuthStatus.SUCCESS
+import com.mytests.testExam.presentation.profile.view_model.UserEvent
 import com.mytests.testExam.presentation.profile.view_model.UserVM
 import com.mytests.testExam.presentation.specialViews.UserNameTF
 import com.mytests.ui.customItems.*
 import com.mytests.ui.screenRoutes.ExamScreenRoutes
 
 @Composable
-fun Registration(controller: NavHostController,userVM: UserVM) {
+fun Registration(controller : NavHostController, userVM : UserVM) {
     val userData = userVM.resultSF.collectAsState().value
     var userName by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
@@ -32,7 +34,7 @@ fun Registration(controller: NavHostController,userVM: UserVM) {
     Column(Modifier.fillMaxSize(),Arrangement.Center,Alignment.CenterHorizontally) {
         FredTextHeader(stringResource(string.registration))
         Spacer(Modifier.height(64.dp))
-        UserNameTF(userName,{ userName = it },isUserNameCorrect)
+        UserNameTF(userName, { userName = it }, isUserNameCorrect)
         Spacer(Modifier.height(8.dp))
         CustomOutlinedTF(
             password,
@@ -52,20 +54,23 @@ fun Registration(controller: NavHostController,userVM: UserVM) {
             errorId = string.incorrectEmail
         )
         Spacer(Modifier.height(8.dp))
-        FredTextField(name,{ name = it },stringResource(string.enterName))
+        FredTextField(name, { name = it }, stringResource(string.enterName))
         Spacer(Modifier.height(8.dp))
-        FredTextField(surname,{ surname = it },stringResource(string.enterSurname))
+        FredTextField(surname, { surname = it }, stringResource(string.enterSurname))
         Spacer(Modifier.height(32.dp))
         FredButton({
             isUserNameCorrect = userName.isBlank()
             isPasswordCorrect = password.isBlank() || password.length < 8
             isEmailCorrect = email.isBlank() || !email.contains("@")
-            if(!isEmailCorrect && !isUserNameCorrect && !isPasswordCorrect) userVM.registration(User(userName,password,email,name,surname))
+            if(!isEmailCorrect && !isUserNameCorrect && !isPasswordCorrect) userVM.onUserEvent(UserEvent.Registration(User(userName, password, email, name, surname)))
         },stringResource(string.signUp))
         Spacer(Modifier.height(8.dp))
         FredButton({ controller.navigate(ExamScreenRoutes.Authorization.route) },stringResource(string.goBack))
         Spacer(Modifier.height(8.dp))
-        if(userData.first == SUCCESS) controller.navigate(ExamScreenRoutes.Profile.route)
-        if(userData.first == EXISTING_USER) FredText(stringResource(string.existingUser))
+        when(userData.status) {
+            SUCCESS -> controller.navigate(ExamScreenRoutes.Profile.route)
+            EXISTING_USER -> FredText(stringResource(string.existingUser))
+            else -> {}
+        }
     }
 }
