786b281eb906b56b44a733435f940fce14cbe0b7
FredNekrasov <frednekrasov49@gmail.com>
2024-05-13 19:34:19 +0300

    refactoring the data and domain DI modules

--- app/src/main/java/com/mytests/testExam/di/DataExamModule.kt
+++ app/src/main/java/com/mytests/testExam/di/DataExamModule.kt
@@ -2,28 +2,24 @@ package com.mytests.testExam.di
 
 import androidx.room.Room
 import com.mytests.testExam.data.local.ExamDb
-import com.mytests.testExam.data.local.dao.IAnimalFactsDao
-import com.mytests.testExam.data.local.dao.IUserDao
-import com.mytests.testExam.data.remote.service.IAnimalFactsService
-import com.mytests.testExam.data.repository.AnimalFactsRepository
-import com.mytests.testExam.data.repository.UserRepository
-import com.mytests.testExam.domain.repository.IAnimalFactsRepository
-import com.mytests.testExam.domain.repository.IUserRepository
+import com.mytests.testExam.data.remote.service.AnimalFactsClient
+import com.mytests.testExam.data.repository.*
+import com.mytests.testExam.domain.repository.*
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 val dataExamModule = module {
     single {
-        Retrofit.Builder().baseUrl(IAnimalFactsService.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(IAnimalFactsService::class.java)
+        AnimalFactsClient.client
     }
-    single<ExamDb> { Room.databaseBuilder(get(), ExamDb::class.java, ExamDb.DATABASE_NAME).allowMainThreadQueries().build() }
-    single<IUserDao> { get<ExamDb>().userDao }
-    single<IAnimalFactsDao> { get<ExamDb>().animalFactsDao }
-
-    single<IAnimalFactsRepository> { AnimalFactsRepository(get(), get()) }
-    single<IUserRepository> { UserRepository(get()) }
+    single<ExamDb> {
+        Room.databaseBuilder(get(), ExamDb::class.java, ExamDb.DATABASE_NAME)
+            .allowMainThreadQueries()
+            .build()
+    }
+    single<IAnimalFactsRepository> {
+        AnimalFactsRepository(get<ExamDb>().animalFactsDao, get())
+    }
+    single<IUserRepository> {
+        UserRepository(get<ExamDb>().userDao)
+    }
 }
--- app/src/main/java/com/mytests/testExam/di/DomainExamModule.kt
+++ app/src/main/java/com/mytests/testExam/di/DomainExamModule.kt
@@ -1,27 +1,16 @@
 package com.mytests.testExam.di
 
-
-import com.mytests.testExam.domain.useCases.animalFacts.AnimalFactsUseCases
-import com.mytests.testExam.domain.useCases.animalFacts.get.IGetAnimalFactsUseCase
-import com.mytests.testExam.domain.useCases.animalFacts.get.implementation.GetAnimalFactsUseCase
-import com.mytests.testExam.domain.useCases.animalFacts.update.IUpdateFactUseCase
-import com.mytests.testExam.domain.useCases.animalFacts.update.implementation.UpdateFactUseCase
-import com.mytests.testExam.domain.useCases.user.UserUseCases
-import com.mytests.testExam.domain.useCases.user.authorization.IAuthorizationUseCase
-import com.mytests.testExam.domain.useCases.user.authorization.implementation.AuthorizationUseCase
-import com.mytests.testExam.domain.useCases.user.delete.IDeleteUserUseCase
-import com.mytests.testExam.domain.useCases.user.delete.implementation.DeleteUserUseCase
-import com.mytests.testExam.domain.useCases.user.registration.IRegistrationUseCase
-import com.mytests.testExam.domain.useCases.user.registration.implementation.RegistrationUseCase
+import com.mytests.testExam.domain.useCases.animalFacts.*
+import com.mytests.testExam.domain.useCases.user.*
 import org.koin.dsl.module
 
 val domainExamModule = module {
-    factory<IAuthorizationUseCase> { AuthorizationUseCase(get()) }
-    factory<IRegistrationUseCase> { RegistrationUseCase(get()) }
-    factory<IDeleteUserUseCase> { DeleteUserUseCase(get()) }
+    factory { AuthorizationUseCase(get()) }
+    factory { RegistrationUseCase(get()) }
+    factory { DeleteUserUseCase(get()) }
     factory { UserUseCases(get(), get(), get()) }
 
-    factory<IGetAnimalFactsUseCase> { GetAnimalFactsUseCase(get()) }
-    factory<IUpdateFactUseCase> { UpdateFactUseCase(get()) }
+    factory { GetAnimalFactsUseCase(get()) }
+    factory { UpdateFactUseCase(get()) }
     factory { AnimalFactsUseCases(get(), get()) }
 }
