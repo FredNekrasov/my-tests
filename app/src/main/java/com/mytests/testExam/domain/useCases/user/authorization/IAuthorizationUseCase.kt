ce608367c7c7d91b688d934f8d94ddf5cd909cbb
FredNekrasov <frednekrasov49@gmail.com>
2024-05-13 19:21:10 +0300

    removing unnecessary use case interfaces

--- app/src/main/java/com/mytests/testExam/domain/useCases/user/authorization/IAuthorizationUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.mytests.testExam.domain.useCases.user.authorization
-
-import com.mytests.testExam.domain.util.UserData
-
-interface IAuthorizationUseCase {
-    suspend fun authorization(userName : String, password : String): UserData
-}
--- app/src/main/java/com/mytests/testExam/domain/useCases/user/delete/IDeleteUserUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.mytests.testExam.domain.useCases.user.delete
-
-import com.mytests.testExam.domain.model.User
-import com.mytests.testExam.domain.util.UserData
-
-interface IDeleteUserUseCase {
-    suspend fun deleteUser(user: User?): UserData
-}
