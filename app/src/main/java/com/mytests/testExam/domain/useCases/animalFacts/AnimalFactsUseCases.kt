8ae22ca8351c67d1ad17371d04071dbac58538c3
FredNekrasov <frednekrasov49@gmail.com>
2024-05-13 19:28:22 +0300

    refactoring the UserUseCases and AnimalFactsUseCases

--- app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/AnimalFactsUseCases.kt
+++ app/src/main/java/com/mytests/testExam/domain/useCases/animalFacts/AnimalFactsUseCases.kt
@@ -1,9 +1,6 @@
 package com.mytests.testExam.domain.useCases.animalFacts
 
-import com.mytests.testExam.domain.useCases.animalFacts.get.IGetAnimalFactsUseCase
-import com.mytests.testExam.domain.useCases.animalFacts.update.IUpdateFactUseCase
-
 data class AnimalFactsUseCases(
-    val getAnimalFacts: IGetAnimalFactsUseCase,
-    val updateAnimalFact: IUpdateFactUseCase
+    val getAnimalFacts: GetAnimalFactsUseCase,
+    val updateAnimalFact: UpdateFactUseCase
 )
--- app/src/main/java/com/mytests/testExam/domain/useCases/user/UserUseCases.kt
+++ app/src/main/java/com/mytests/testExam/domain/useCases/user/UserUseCases.kt
@@ -1,11 +1,7 @@
 package com.mytests.testExam.domain.useCases.user
 
-import com.mytests.testExam.domain.useCases.user.authorization.IAuthorizationUseCase
-import com.mytests.testExam.domain.useCases.user.delete.IDeleteUserUseCase
-import com.mytests.testExam.domain.useCases.user.registration.IRegistrationUseCase
-
 data class UserUseCases(
-    val authorizationUseCase : IAuthorizationUseCase,
-    val registrationUseCase : IRegistrationUseCase,
-    val deleteUserUseCase : IDeleteUserUseCase
+    val authorizationUseCase : AuthorizationUseCase,
+    val registrationUseCase : RegistrationUseCase,
+    val deleteUserUseCase : DeleteUserUseCase
 )
