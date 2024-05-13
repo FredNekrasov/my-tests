e3b0c4b2d73586585a15060b945f23c0c6a40ec9
FredNekrasov <frednekrasov49@gmail.com>
2024-05-13 19:31:03 +0300

    renaming the IAnimalFactsService.kt to the AnimalFactsClient

---
+++ app/src/main/java/com/mytests/testExam/data/remote/service/AnimalFactsClient.kt
@@ -0,0 +1,29 @@
+package com.mytests.testExam.data.remote.service
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object AnimalFactsClient {
+    val client = HttpClient(OkHttp) {
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                }
+            )
+        }
+        defaultRequest {
+            url("https://cat-fact.herokuapp.com")
+        }
+    }
+}
